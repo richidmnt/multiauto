68098574a98ed7b63a0a570f673cf5967cbec20b
richidmnt <richi1998-@hotmail.com>
2024-08-11 00:43:31 -0500

    create super user

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'prueba123')
+EOF
